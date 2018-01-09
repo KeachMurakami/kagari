26122f92a30e373df3f15894061147a8fed7bf01
MurakamiKeach <m.keach.11@gmail.com>
2018-01-09 10:38:51 +0900

    add tentative function: read junior-pam (and mini-pam) data

---
+++ R/junior_pam.R
@@ -0,0 +1,15 @@
+read_junior_pam <-
+  function(file_path, ...){
+
+    data_body <-
+      file_path %>%
+      readr::read_csv(skip = 1) %>%
+      slice(-1)
+
+    data_body %>%
+      dplyr::select(date = Date, hhmmss = Time, type = Type,
+                    ppfd = `1:PAR`, y2 = `1:Y (II)`,
+                    fm = `1:Fm'`, f = `1:F`, ...) %>%
+      dplyr::mutate(time = paste(date, hhmmss),
+                    time = lubridate::ymd_hms(time))
+  }
